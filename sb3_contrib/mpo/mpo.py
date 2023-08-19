7ae4dbfd0776d04c8a4b41ee0c9af5edb71c92cc
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-08-20 00:31:32 +0200

    Pseudo code-ish notes of what Acme and tonic do

--- sb3_contrib/mpo/mpo.py
+++ sb3_contrib/mpo/mpo.py
@@ -157,22 +157,6 @@ class MPO(OffPolicyAlgorithm):
 
     def train(self, gradient_steps: int, batch_size: int = 100) -> None:
         # TODO Main todo
-        # TODO Somewhere compute evidence lower bound (ELBO), symbol fancy J, Equation (2 in paper)
-        # TODO E-step Improve ELBO w.r.t. q using off-policy deep RL techniques and value-function approximation
-        #      -> optimises J w.r.t. to q
-        #      --> partial maximization of J(q, θ) with respect to q given θ = θi
-        # TODO M-step Update the parametric policy in a supervised learning step using the reweighted state-action samples from
-        #      the E-step as targets
-        #      -> optimises J w.r.t. to pi
-
-        # TODO E-step: sample from replay buffer, compute Q-values, compute ELBO (GitHub Copilot suggestion)
-        # TODO M-step: optimize ELBO w.r.t. policy parameters (GitHub Copilot suggestion)
-
-        # TODO E-step
-        # TODO ---------------------------------------------------------------------
-        # TODO We start by setting q = πθ
-        # TODO Estimate the unregularized action-value function (in practice we estimate Qθ from off-policy data (we refer to
-        #      Section 4 for details about the policy evaluation step))
 
         # Switch to train mode (this affects batch norm / dropout)
         self.policy.set_training_mode(True)
@@ -181,12 +165,93 @@ class MPO(OffPolicyAlgorithm):
         self._update_learning_rate([self.actor.optimizer, self.critic.optimizer])
 
         actor_losses, critic_losses = [], []
-        for _ in range(gradient_steps):
+        for _ in range(gradient_steps):  # TODO: Are there multiple gradient steps in Acme? (I think there are higher up)
             self._n_updates += 1
             # Sample replay buffer
             replay_data = self.replay_buffer.sample(batch_size, env=self._vec_normalize_env)  # type: ignore[union-attr]
 
+            # PSEUDO CODE (following Acme)
+            # ----------------------
+            # -> Get action distribution from policy based on based on "next observation" (o_t) from replay buffer
+            # -> Get action distribution from target policy based on based on "next observation" (o_t) from replay buffer
+            # -> Sample n actions from target action distribution ("to evaluate policy")
+            # -> Compute the target critic's Q-value of the sampled actions in state o_t
+            # -> Average target critic Q-values to compute the TD-learning bootstrap target (before this reshape to batch dims)
+            # -> Compute online critic value of previous observation (o_t-1) and action (a_t-1) from replay buffer
+            # -> Compute critic loss as TD(0) error (and average it)
+            # -> Compute policy loss (through policy loss module from online and target action distributions, sampled actions
+            #    and the latter's Q-values)
+            #   MPO loss with decoupled KL constraints as in (Abdolmaleki et al., 2018)
+            #   ----------------------
+            #   -> Multivariate normal distribution of target and online action to independent normals
+            #   -> Clip (logs) dual variables (temperature, alpha_mean and alpha_std) to (-18, None) to ensure they stay
+            #      positive (are alphas Lagrange multipliers?)
+            #   -> Transform dual variables from log to normal space using softplus instead of exp (for numerical stability)
+            #      ... (and +1e-8 ... why?)
+            #   -> Compute normalized importance weights, used to compute expectations with respect to the non-parametric
+            #      policy; and the temperature loss, used to adapt the tempering of Q-values
+            #      (computes_weights_and_temperature_loss function)
+            #  (->) FOR DIAGNOSTICS: Compute estimated actualized KL between the non-parametric and current target policies
+            #   -> IF PENALIZED_ACTIONS option is activated:
+            #     -> SOME OTHER ADDITIONAL STUFF that adds to normalized weights and temperature loss
+            #   -> Decompose the online policy into fixed-mean & fixed-stddev distributions (indepenent normals with target
+            #      mean and online stddev, and vice versa) ... as per second paper works better
+            #   -> Compute decomposed policy losses using each of the decomposed distributions (using passed actions,
+            #      normalized weights and the respoective distribution)
+            #   -> Compute the decomposed KL divergence between the target and online policies (KL divergences from target
+            #      action distribution to either of the decombosed distributions) / (slightly different behaviour based on
+            #      per_dim_constraining option)
+            #   -> Compute the alpha-weighted KL-penalty and dual losses to adapt the alphas (loss_[kl|alpha]_[mean|std] using
+            #      kl_[mean|std], alpha_[mean|std] and epsilon_[mean|std] ... epilson being a config option) ...
+            #      (compute_parametric_kl_penalty_and_dual_loss function)
+            #   -> Combine (sum up) losses policy and KL penalty mean and std ... dual loss made up of alpha mean and std, and
+            #      temperature loss ... then all three summed (loss = loss_policy + loss_kl_penalty + loss_dual)
+            #   ---------------------- (returns loss)
+            # -> Clip gradients of critic and policy (and dual variables) ... (by global norm? 40.0?)
+            # -> Apply optimisers to critic, policy and dual variables
+
+            # PSUEDO CODE (following tonic) ... x means "no grad"
+            # ----------------------
+            # -> x Get target action distribution from target actor (from observations passed to updater (from replay
+            #      buffer???))
+            # -> x Sample n actions from target action distribution
+            # -> x Compute the target critic's Q-value of the sampled actions using the passed observations
+            # -> x Convert target action distribution from normal to independent normals to satisfy KL constraints
+            #      per-dimension
+            # -> Zero grad for actor optimiser dual optimiser
+            # -> Get action distribution from (non-target) actor ... then convert to independent normals
+            # -> Get dual variables (temperature, alpha_mean and alpha_std) from their logs using softplus and adding
+            #    FLOAT_EPSILON = 1e-8
+            # -> weights_and_temperature_loss function to compute weights and temperature loss from previously computed target
+            #    critic values, self.episolon and temperature
+            # -> IF ACTION_PENALIZATION option is activated:
+            #   -> SOME OTHER ADDITIONAL STUFF
+            # -> Decompose the policy (target action distribution from earlier) into fixed-mean and fixed-std distributions
+            #    mean from non-target action distribution and std from target action distribution, and vice versa (as
+            #    independent normals)
+            # -> Compute the decomposed policy losses (policy_mean_loss and policy_std_loss)
+            # -> Compute the decomposed KL divergences between the target and online policies (kl_mean and kl_std) ... slighty
+            #    different behaviour based on per_dim_constraining option
+            # -> Compute the alpha-weighted KL-penalty and dual losses (kl_[mean|std]_loss and alpha_[mean|std]_loss) ... based
+            #    on kl_[mean|std], alpha_[mean|std] and epsilon_[mean|std] ... epsilon being a config option
+            # -> Combine (sum up) losses:
+            #      - policy loss (policy_mean_loss + policy_std_loss)
+            #      - KL (penalty) loss (kl_mean_loss + kl_std_loss)
+            #      - dual loss (alpha_mean_loss + alpha_std_loss + temperature_loss)
+            #      -----------------------------------------
+            #      - total loss (policy_loss + kl_loss + dual_loss)
+            # -> Backpropagate loss ... and IF gradient_clip (OPTION?) is True: clip_grad_norm for actor variables and dual
+            #    varialbes
+            # -> Step optimisers (actor optimiser and dual optimiser)
+
             with th.no_grad():
+                # TODO: Get action distribution from policy based on based on "next observation" from replay buffer
+                next_action_distribution = self.actor(replay_data.next_observations)
+                # TODO: Get action distribution from target policy based on based on "next observation" from replay buffer
+                next_action_distribution_target = self.actor_target(replay_data.next_observations)
+
+                # ----------------------
+
                 # Select action according to policy and add clipped noise
                 noise = replay_data.actions.clone().data.normal_(0, self.target_policy_noise)
                 noise = noise.clamp(-self.target_noise_clip, self.target_noise_clip)
@@ -221,6 +286,7 @@ class MPO(OffPolicyAlgorithm):
                 actor_loss.backward()
                 self.actor.optimizer.step()
 
+                # TODO: Is it Polyak update in Acme or just a copy?
                 polyak_update(self.critic.parameters(), self.critic_target.parameters(), self.tau)
                 polyak_update(self.actor.parameters(), self.actor_target.parameters(), self.tau)
                 # Copy running stats, see GH issue #996
